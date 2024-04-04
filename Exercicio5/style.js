252309747a802f6574995fcdf32a98a817dde117
guilherme8l <101971954+Ilhe8l@users.noreply.github.com>
2024-04-04 10:36:06 -0300

    Create style.js

---
+++ Exercicio5/style.js
@@ -0,0 +1,27 @@
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=sawp");
+
+body{
+    font-size: 1.2rem;
+    background-color: #FEE7AA;
+    display: flex;
+    flex-direction: column;
+    min-height: 85vh;
+    font-family: 'Poppins', sans-serif;
+    justify-content: center;
+    align-items: center;
+}
+
+
+.wrapper{
+    width: 420px;
+    background-color: #FDD771;
+    color: #423207;
+    border-radius: 10px;
+    padding: 10px 40px;
+}
+
+.wrapper h1{
+    margin: 10px auto;
+    font-size: 36px;
+    text-align: center;
+}
